7288b4f4d2dd6eb9b6ea07777e1a0eb04ae703b7
swimngolf <swimngolf@users.noreply.github.com>
2026-04-03 15:13:27 -0400

    Add PWA support: installable as iPhone app

---
+++ sw.js
@@ -0,0 +1,15 @@
+﻿const CACHE_NAME = 'streak-survivor-v1';
+const ASSETS = ['/survivor-dashboard/', '/survivor-dashboard/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
